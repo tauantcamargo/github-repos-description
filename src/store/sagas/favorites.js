9621a3279bd80c39a3b31ca4e489d0e3d232524f
lukyhenson <lukyhenson@gmail.com>
2019-06-27 01:02:58 -0300

    it prevents an error when user try to add duplicated repo

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
 import api from '../../services/api';
@@ -7,14 +7,23 @@ export function* addFavorite(action) {
  try {
   const { data } = yield call(api.get, `repos/${action.payload.repository}`);
 
-  const repositoryData = {
-    id: Math.random(),
-    name: data.full_name,
-    description: data.description,
-    url: data.html_url,
-  }
+  const isDuplicated = yield select(state => (
+    state.favorites.data.find(favorite => favorite.id === data.id)
+  ));
 
-  yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+  if (isDuplicated) {
+    yield put(FavoriteActions.addFavoriteFailure("Error to add this repository! Duplicated Repository D:"));
+  } else {
+    const repositoryData = {
+      id: data.id,
+      name: data.full_name,
+      description: data.description,
+      url: data.html_url,
+    }
+
+    yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+
+  }
  } catch (err) {
   yield put(FavoriteActions.addFavoriteFailure("Error to add this repository! Try againg later :D"));
  }
